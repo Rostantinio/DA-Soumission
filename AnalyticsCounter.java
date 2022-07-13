3b11c2043bb45f107865aa7326cbfed7ba78c547
Kamdem guy rostant <guyrostantkamdem@gmail.com>
2022-07-13 12:21:10 +0100

    ecriture dans le fichier

--- AnalyticsCounter.java
+++ AnalyticsCounter.java
@@ -7,8 +7,10 @@ import java.util.*;
 public class AnalyticsCounter {
 
 	public static void main(String args[]) throws IOException {
-		//comptage du nombre d'occurrences de chaque symptome
-		ISymptomsCount counter = new SymptomsCounter();
-		Map<String, Integer> symptomsWithOccurrences = counter.countSymptoms(symptoms);
+		//ecriture dans le fichier de sortie
+		ISymptomWrite symptomWrite = new WriteSymptomDataToFile("result.out");
+		symptomWrite.writeToFile(symptomsWithOccurrences);
+
+
 	}
 }
